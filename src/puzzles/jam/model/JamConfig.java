ee7ecc29c6210b6eb4af23fa18088dc3273f73b2
Cole Brindisi <cab9337@g.rit.edu>
2022-11-18 13:38:55 -0500

    Preliminary Solver done

--- src/puzzles/jam/model/JamConfig.java
+++ src/puzzles/jam/model/JamConfig.java
@@ -1,34 +1,185 @@
 package puzzles.jam.model;
 
-// TODO: implement your JamConfig for the common solver
 
 import puzzles.common.solver.Configuration;
+import puzzles.jam.solver.Jam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class JamConfig implements Configuration {
+
+    private final char[] car;
+    private final int[] startRow;
+    private final int[] startCol;
+
+    private final int[] endRow;
+    private final int[] endCol;
+    private final int goal;
+    private final char[][] board;
+
+    private final boolean[] isVert;
+    private final boolean[] isHorizontal;
+
+    public JamConfig(char[][] board, char[] car, int[] startRow, int[] startCol, int[] endRow, int[] endCol, int goal) {
+        this.car = car;
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.endRow = endRow;
+        this.endCol = endCol;
+        this.goal = goal;
+        this.board = board;
+        this.isVert = new boolean[car.length];
+        this.isHorizontal = new boolean[car.length];
+
+        for (int i = 0; i < car.length; i++) {
+            if (startRow[i] == endRow[i]) {
+                isHorizontal[i] = true;
+            } else if (startCol[i] == endCol[i]){
+                isVert[i] = true;
+            }
+        }
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
     @Override
     public boolean isSolution() {
+        for (char[] chars : board) {
+            if (chars[goal] == 'X') {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private char[][] copyBoard(char[][] other) {
+        char[][] newOne = new char[other.length][other[0].length];
+        for (int i = 0; i < other.length; i++) {
+            System.arraycopy(other[i], 0, newOne[i], 0, other[0].length);
+        }
+        return newOne;
     }
 
     @Override
     public Collection<Configuration> getNeighbors() {
-        return null;
+        Collection<Configuration> neighbors = new ArrayList<>();
+
+        // For every car on the board
+        for (int i = 0; i < car.length; i++) {
+            if (isHorizontal[i]) {
+
+                // If there is an empty space to the right
+                if (endCol[i] + 1 < board[0].length
+                        && board[startRow[i]][endCol[i] + 1] == '.') {
+
+                    // Make local variables
+                    char[][] horizAdd = copyBoard(board);
+                    int[] rightStartCol = startCol.clone();
+                    int[] rightEndCol = endCol.clone();
+
+                    // Move car to the right one and create new neighbor
+                    horizAdd[startRow[i]][rightStartCol[i]] = '.';
+                    rightStartCol[i]++;
+                    rightEndCol[i]++;
+                    horizAdd[startRow[i]][rightEndCol[i]] = car[i];
+                    neighbors.add(new JamConfig(
+                            horizAdd, car, startRow, rightStartCol, endRow, rightEndCol, goal));
+                }
+
+                // If there is an empty space to the left
+                if (startCol[i] > 0
+                        && board[startRow[i]][startCol[i] - 1] == '.') {
+
+                    // Make local variables
+                    char[][] horizSubtract = copyBoard(board);
+                    int[] leftStartCol = startCol.clone();
+                    int[] leftEndCol = endCol.clone();
+
+                    // Move car to the left one and create new neighbor
+                    horizSubtract[startRow[i]][leftEndCol[i]] = '.';
+                    leftStartCol[i]--;
+                    leftEndCol[i]--;
+                    horizSubtract[startRow[i]][leftStartCol[i]] = car[i];
+                    neighbors.add(new JamConfig(
+                            horizSubtract, car, startRow, leftStartCol, endRow, leftEndCol, goal));
+                }
+
+            } else if (isVert[i]) {
+
+                // If there is an empty space below
+                if (endRow[i] + 1 < board.length
+                        && board[endRow[i] + 1][endCol[i]] == '.') {
+
+                    // Make local variables
+                    char[][] vertAdd = copyBoard(board);
+                    int[] downStartRow = startRow.clone();
+                    int[] downEndRow = endRow.clone();
+
+                    // Move car down one and create new neighbor
+                    vertAdd[downStartRow[i]][startCol[i]] = '.';
+                    downStartRow[i]++;
+                    downEndRow[i]++;
+                    vertAdd[downEndRow[i]][startCol[i]] = car[i];
+                    neighbors.add(new JamConfig(
+                            vertAdd, car, downStartRow, startCol, downEndRow, endCol, goal));
+                }
+
+                // If there is an empty space above
+                if (startRow[i] > 0
+                        && board[startRow[i] - 1][endCol[i]] == '.') {
+
+                    // Make local variables
+                    char[][] vertAdd = copyBoard(board);
+                    int[] upStartRow = startRow.clone();
+                    int[] upEndRow = endRow.clone();
+
+                    // Move car up one and create new neighbor
+                    vertAdd[upEndRow[i]][startCol[i]] = '.';
+                    upStartRow[i]--;
+                    upEndRow[i]--;
+                    vertAdd[upStartRow[i]][startCol[i]] = car[i];
+                    neighbors.add(new JamConfig(
+                            vertAdd, car, upStartRow, startCol, upEndRow, endCol, goal));
+                }
+            }
+        }
+
+        return neighbors;
     }
 
     @Override
     public boolean equals(Object other) {
+        if (other instanceof JamConfig) {
+            return Arrays.deepEquals(board, ((JamConfig) other).getBoard());
+        }
         return false;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return Arrays.deepHashCode(board);
     }
 
     @Override
     public String toString() {
-        return null;
+        String end = "";
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (j == board[0].length - 1) {
+                    end += board[i][j];
+                } else {
+                    end += board[i][j] + " ";
+                }
+            }
+            if (i != board.length - 1) {
+                end += "\n";
+            }
+        }
+        return end;
     }
 }
--- src/puzzles/jam/solver/Jam.java
+++ src/puzzles/jam/solver/Jam.java
@@ -1,11 +1,121 @@
 package puzzles.jam.solver;
 
+import puzzles.common.solver.Configuration;
 import puzzles.common.solver.Solver;
+import puzzles.jam.model.JamConfig;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class Jam {
     public static void main(String[] args) {
+
+        int rows;
+        int cols;
+        int numCars;
+
         if (args.length != 1) {
             System.out.println("Usage: java Jam filename");
+        } else {
+            try (BufferedReader in = new BufferedReader(new FileReader(args[0]))) {
+
+                String[] fields = in.readLine().split("\\s+");
+                rows = Integer.parseInt(fields[0]);
+                cols = Integer.parseInt(fields[1]);
+                numCars = Integer.parseInt(in.readLine());
+
+                char[] cars = new char[numCars];
+                int[] startRows = new int[numCars];
+                int[] startCols = new int[numCars];
+                int[] endRows = new int[numCars];
+                int[] endCols = new int[numCars];
+                char[][] board = new char[rows][cols];
+
+                for (char[] row: board) {
+                    Arrays.fill(row, '.');
+                }
+
+                // For each car in file
+                for (int i = 0; i < numCars; i++) {
+
+                    // Store information of cars in each index
+                    fields = in.readLine().split("\\s+");
+                    cars[i] = fields[0].charAt(0);
+                    startRows[i] = Integer.parseInt(fields[1]);
+                    startCols[i] = Integer.parseInt(fields[2]);
+                    endRows[i] = Integer.parseInt(fields[3]);
+                    endCols[i] = Integer.parseInt(fields[4]);
+
+                    // Fill the board with cars
+                    if (startRows[i] == endRows[i]) {
+                        for (int j = 0; j <= endCols[i] - startCols[i]; j++) {
+                            board[startRows[i]][startCols[i] + j] = cars[i];
+                        }
+                    } else if (startCols[i] == endCols[i]) {
+                        for (int j = 0; j <= endRows[i] - startRows[i]; j++) {
+                            board[startRows[i] + j][startCols[i]] = cars[i];
+                        }
+                    }
+                }
+
+                // Print initial state
+                System.out.println(args[0]);
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < cols; j++) {
+                        if (j == cols - 1) {
+                            System.out.print(board[i][j]);
+                        } else {
+                            System.out.print(board[i][j] + " ");
+                        }
+                    }
+                    System.out.println();
+                }
+
+                // Start solving puzzle
+                JamConfig config = new JamConfig(board, cars, startRows, startCols, endRows, endCols, cols - 1);
+                Solver b = new Solver();
+                int x = 0;
+
+                Collection<Configuration> end = new ArrayList<>(b.solve(config));
+                int size = end.size();
+                boolean endResult = false;
+
+
+                System.out.println("Total configs: " + b.getConfigurations());
+                System.out.println("Unique configs: " + b.getUniqueConfigurations());
+
+                // Print if no solution
+                for (Configuration result: end) {
+                    if (x == size - 1) {
+                        if (result.isSolution()) {
+                            endResult = true;
+                        } else {
+                            System.out.println("No solution");
+                        }
+
+                    }
+                    x++;
+                }
+
+                int step = 0;
+
+                // Print path
+                if (endResult) {
+                    for (Configuration boards: end) {
+                        System.out.println("Step " + step + ":");
+                        System.out.println(boards);
+                        System.out.println();
+                        step++;
+                    }
+                }
+
+            } catch (IOException e) {
+                System.out.println("Invalid File");
+            }
         }
     }
 }
